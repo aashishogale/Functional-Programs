9852cf9b6afa7862b2399164c34a47ff8a3d2176
aashishogale <aashishogale@gmail.com>
2017-10-04 15:49:57 +0530

    commit message

--- com/bridgelabz/lib/TicTacToe.java
+++ com/bridgelabz/lib/TicTacToe.java
@@ -137,6 +137,20 @@ public class TicTacToe {
 
 	}
 
+	public static boolean drawCheck() {
+		
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				if(a[i][j]=='-';) {
+				return false;
+				
+				}
+
+			}
+		}
+		return true		
+	}
+
 	public static  void display() {
 	
 		for(int i=0;i<3;i++) {
